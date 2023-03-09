f8af939c793c6e166c5c352ec8ba14522d55710c
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-03-09 20:54:57 +0430

    Add the project script.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,27 @@
 import generateJoke from './generateJoke.js';
 import './styles/main.scss';
 import todoIcom from './assets/todo-list.svg';
+import UI from './modules/ui.js';
 
-// Define the to-do tasks as an array of objects
-const tasks = [
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Do laundry',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Clean the house',
-    completed: false,
-    index: 2,
-  },
-];
-
-// Get a reference to the HTML ul element
-const todoList = document.getElementById('todo-list');
-
-// Loop over the tasks array and create an HTML li element for each task
-tasks.forEach((task) => {
-  // Create a new li element
-  const listItem = document.createElement('li');
-
-  // Set the text content of the li element to the task description
-  const span = document.createElement('span');
-  span.textContent = task.description;
-
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  listItem.appendChild(checkbox);
-  listItem.appendChild(span);
-
-  // If the task is completed, add a "completed" class to the li element
-  if (task.completed) {
-    listItem.classList.add('completed');
-  }
-
-  // Add the li element to the ul element
-  todoList.appendChild(listItem);
-});
-
+// Todo List Icon
 const laughImg = document.getElementById('laughImg');
-
 laughImg.src = todoIcom;
 
-generateJoke();
+// Event: Display Tasks
+document.addEventListener('DOMContentLoaded', UI.displaytasks);
 
+// Event: Add a task
+document.querySelector('#add-form').addEventListener('keypress', (e) => {
+  // prevent default submit
+
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    // get form values
+    UI.getNewTask();
+  }
+});
+
+// Remove later
+generateJoke();
 const jokeBtn = document.getElementById('jokBtn');
 jokeBtn.addEventListener('click', generateJoke);
